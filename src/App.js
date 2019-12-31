5f9067680123e5770cd22b29232482833bd276b3
Yuqi Yang <yuqiyang@Yuqi-Yangs-MacBook-Pro.local>
2019-12-30 21:44:33 -0500

    onClick onChange

--- src/App.js
+++ src/App.js
@@ -25,6 +25,16 @@ class App extends Component{
   }
 
 
+  nameChangeHandler=(e) =>{
+    this.setState({
+      persons: [
+        { name: 'max', age:28 },
+        { name: e.target.value, age:29 },
+        { name: 'yu', age:27 }
+      ]
+    })
+  }
+
   render(){
     return(
       <div classname="App">
@@ -32,10 +42,13 @@ class App extends Component{
         <p>this is really working</p>
         <button onClick={()=>this.switchNameHandler("hlllll")}>Switch name</button>
         <Person name={this.state.persons[0].name} age ={this.state.persons[0].age} />
+        
         <Person 
         name ={this.state.persons[1].name} 
         age ={this.state.persons[1].age}
-        click={this.switchNameHandler}> i like icecream </Person>
+        click={this.switchNameHandler}
+        changed={this.nameChangeHandler}> i like icecream </Person>
+        
         <Person name ={this.state.persons[2].name} age ={this.state.persons[2].age}/>
       </div>
     );//we can pass method in add.js also as props 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,6 +5,7 @@ return (
     <div>
         <p onClick={props.click}>I am {props.name} and i am {props.age} years old </p>
         <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name} />
     </div>
 )
 }
